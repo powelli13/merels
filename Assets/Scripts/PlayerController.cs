c7e5f268a335314deae1eaaeee67db35cfcd71d2
Isaac Powell <powelli91@outlook.com>
2026-05-28 19:41:03 -0700

    added tutorial assets. created basic script while following tutorial.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 position = transform.position;
+        position.x = position.x + 0.0001f;
+        position.y = position.y + 0.0002f;
+        transform.position = position;
+    }
+}
