131c852227b36a6a955c080f50e9cff851035545
Isaac Powell <powelli91@outlook.com>
2026-06-20 17:28:41 -0700

    working through memory game example and learning

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -1,8 +1,15 @@
 using System.Data.Common;
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class SceneController : MonoBehaviour
 {
+    public const int gridRows = 2;
+    public const int gridCols = 4;
+    public const float offsetX = 2f;
+    public const float offsetY = 2.5f;
+
     [SerializeField]
     MemoryCard originalCard;
 
@@ -12,8 +19,47 @@ public class SceneController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        int id = Random.Range(0, images.Length);
-        originalCard.SetCard(id, images[id]);
+        Vector3 startPos = originalCard.transform.position;
+
+        int[] numbers = {0, 0, 1, 1, 2, 2, 3, 3};
+        numbers = ShuffleArray(numbers);
+
+        // TODO update styling rules?
+        for (int i = 0; i < gridCols; i++)
+        {
+            for (int j = 0; j < gridRows; j++)
+            {
+                MemoryCard card;
+                if (i == 0 && j == 0)
+                {
+                    card = originalCard;
+                } else {
+                    card = Instantiate(originalCard) as MemoryCard;
+                }
+
+                int index = j * gridCols + i;
+                int id = numbers[index];
+                card.SetCard(id, images[id]);
+
+                float posX = (offsetX * i) + startPos.x;
+                float posY = -(offsetY * j) + startPos.y;
+                card.transform.position = new Vector3(posX, posY, startPos.z);
+            }
+        }
+    }
+
+    private int[] ShuffleArray(int[] numbers)
+    {
+        int[] newArray = numbers.Clone() as int[];
+        for (int i = 0; i < newArray.Length; i++)
+        {
+            int tmp = newArray[i];
+            int r = Random.Range(i, newArray.Length);
+            newArray[i] = newArray[r];
+            newArray[r] = tmp;
+        }
+
+        return newArray;
     }
 
     // Update is called once per frame
