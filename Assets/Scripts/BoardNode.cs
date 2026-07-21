ac9ab2f86e76253eeaf9dd16474de5d834ce8580
Isaac Powell <powelli91@outlook.com>
2026-07-20 22:41:04 -0700

    setting up some wiring for triplet detection and thinking about data structures and state management. putting thoughts down in comments.

--- Assets/Scripts/BoardNode.cs
+++ Assets/Scripts/BoardNode.cs
@@ -5,14 +5,18 @@ public class BoardNode : MonoBehaviour
     [SerializeField]
     GameController gameController;
 
-    // [SerializeField]
-    // int _id;
-
     private int _id;
+    private bool _selected;
 
     public int Id
     {
         get { return _id; }
+    }
+
+    public bool Selected
+    {
+        get { return _selected; }
+        set { _selected = value; }
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -30,7 +34,16 @@ public class BoardNode : MonoBehaviour
     void OnMouseUp()
     {
         Debug.Log("Node clicked with ID: " + _id);
+        Debug.Log("Node is now selected");
         gameController.ReportClick(_id);
+
+        // TODO just messing around with basic plumbing for states
+        // TODO design question: should this selection management be owned by the node or the game controller?
+        // I think it makes more sense to have the board node own it and the game controller check it
+        if (!_selected)
+        {
+            _selected = true;
+        }
     }
 
     public void SetBoardNode(int id)
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -8,32 +8,39 @@ public class GameController : MonoBehaviour
     /**
     TODO board shape diagram with IDs
     */
-    // We give this a length in the GUI, so we can rely on the board shape above
-    // The preset structure of the board allows for quickly checking for 3 in a row scores
-    //[SerializeField]
-    //BoardNode[] boardNodes;
-
     [SerializeField]
     BoardNode originalNode;
+
+    // Map of node IDs to the triplet groups in which they can score.
+    // This is essentially a pre-calculation of all the scoring triplets.
+    // It is helpful because of the structure of the board not being an orthoganal grid.
+    // TODO add more, also mess around with different types, maybe tuples are faster
+    private Dictionary<int, List<int[]>> _scoringTriplets = new()
+    {
+        [0] = new List<int[]> {new int[] {0, 1, 2}, new int[] {0, 4, 7}},
+        [1] = new List<int[]> {new int[] {0, 1, 2}, new int[] {1, 5, 8}},
+        [2] = new List<int[]> {new int[] {0, 1, 2}, new int[] {2, 6, 9}}
+    };
+
+    private Dictionary<int, BoardNode> _nodes = new();
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        SetupBoard();        
+        SetupBoard();
     }
 
     // Set up the board programmatically to static offsets from the center of the screen
     // rather than manually in the GUI.
     void SetupBoard()
     {
+        // The board center
+        Vector3 bc = transform.position;
+
         // offsets
         // first circle: x:2 y:1
         // second circle: x:3.25 y:2.25
         // third circle: x:4.5 y:3.5
-
-        // The board center
-        Vector3 bc = transform.position;
-
         // I use a static board node positions so that the Node IDs can be found reliably
         // in the different positions.
         // This may be overkill but I'll simplify later. For now I like having the board
@@ -90,11 +97,15 @@ public class GameController : MonoBehaviour
 
             node.SetBoardNode(id);
 
+            // TODO is this the cleanest approach?
+            _nodes.Add(id, node);
+
             float posX = nodeOffsets[id].x;
             float posY = nodeOffsets[id].y;
 
             node.transform.position = new Vector3(posX, posY, bc.z);
         }
+
     }
 
     // Update is called once per frame
@@ -107,5 +118,38 @@ public class GameController : MonoBehaviour
     public void ReportClick(int clickedId)
     {
         Debug.Log("From GameControler: BoardNode clicked ID: " + clickedId);
+
+        // TODO trying out different wiring structures for three in a row scoring detections
+        // Check if the movement selected three in a row
+        // TODO this might throw an error on a board reference value, that would mean some setup is broken
+        DetectScore(clickedId);
+    }
+
+    // TODO add piece type in the future
+    private bool DetectScore(int nodeId)
+    {
+        // get potential scores based on the nodeId
+        // determine if all are active or the same player piece
+        // return true on first find
+        // return a bool because we can only score once per turn
+        var possibleMatches = _scoringTriplets[nodeId];
+
+        // I like this structure, maybe try tuples as well though
+        // Should also add guards to avoid nodeIds causing exceptions
+        // Maybe just a little helper function to return only if the node ID exists
+        foreach (var triplet in possibleMatches)
+        {
+            Debug.Log("The triplet is: " + triplet.ToString());
+            foreach (var subId in triplet)
+            {
+                Debug.Log("Sub ID is: " + subId);
+            }
+            //jif ()
+            //j{
+
+            //j}
+        }
+
+        return false;
     }
 }
